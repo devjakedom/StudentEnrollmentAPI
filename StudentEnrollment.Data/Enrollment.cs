4ef4376586f1b84bb6193b94eddff575c5b8f808
jake dombrowski <tilabbrand@gmail.com>
2021-10-27 03:29:01 -0400

    i finishedd all my code. we can all strt to debug in the morning and make this a perfect project

--- StudentEnrollment.Data/Enrollment.cs
+++ StudentEnrollment.Data/Enrollment.cs
@@ -17,7 +17,10 @@ namespace StudentEnrollment.Data
         [ForeignKey(nameof(Student))]
         public int StudentId { get; set; }
         public int StudentGrade { get; set; }
+        public virtual Course Course { get; set; }
         [ForeignKey(nameof(Course))]
         public int CourseId { get; set; }
+
+        public Guid OwnerId { get; set; }
     }
 }
---
+++ StudentEnrollment.Models/EnrollmentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class EnrollmentDetail
+    {
+        public int EnrollmentId { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int StudentGrade { get; set; }
+
+        public int CourseId { get; set; }
+    }
+}
